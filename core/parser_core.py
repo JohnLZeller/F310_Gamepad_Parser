04c47aac51a2697ad0288fbdad382ace754484c9
JohnLZeller <johnlzeller@gmail.com>
2013-02-12 15:22:49 -0800

    Adjusted code to more closely fit pep8

--- core/parser_core.py
+++ core/parser_core.py
@@ -1,63 +1,62 @@
-############ Logitech F310 Gamepad Controller Parser - parser_core.py ##########
+############ Logitech F310 Gamepad Controller Parser - parser_core.py #########
 # Original Author: John Zeller
-# Description: Parser_Core parses data coming from a Logitech F310 Gamepad Controller and
-#	       updates a dictionary of states.
+# Description: Parser_Core parses data coming from a Logitech F310 Gamepad
+#			   Controller and updates a dictionary of states.
 
-### NOTES ###############################################################################
-# 1) LEAVE MODE 'OFF' there is no support in parser_core.py for MODE ON 		#
-# 2) This parser was built using this tutorial as a reference:				#
-#	http://hackshark.com/?p=147#axzz2BHTVXCFz					#
-# 3) Naturally the gamepad sends the following values:					#
-# 	LJ/RJ - Down: 0-127								#
-# 	LJ/RJ - Up: 255-128								#
-# 	LJ/RJ - Left: 255-128								#
-# 	LJ/RJ - Right: 0-127								#
-# 4) States are saved as the following values:						#
-# 	LJ/RJ - Down: 0-(-127)								#
-# 	LJ/RJ - Up: 0-127								#
-# 	LJ/RJ - Left: 0-(-127)								#
-# 	LJ/RJ - Right: 0-127								#
-#########################################################################################
+### NOTES #####################################################################
+# 1) LEAVE MODE 'OFF' there is no support in parser_core.py for MODE ON
+# 2) This parser was built using this tutorial as a reference:
+#	http://hackshark.com/?p=147#axzz2BHTVXCFz
+# 3) Naturally the gamepad sends the following values:
+# 	LJ/RJ - Down: 0-127
+# 	LJ/RJ - Up: 255-128
+# 	LJ/RJ - Left: 255-128
+# 	LJ/RJ - Right: 0-127
+# 4) States are saved as the following values:
+# 	LJ/RJ - Down: 0-(-127)
+# 	LJ/RJ - Up: 0-127
+# 	LJ/RJ - Left: 0-(-127)
+# 	LJ/RJ - Right: 0-127
+###############################################################################
 
-### Buttons/Joys Represented: ###########################################################
-# A, B, X, Y, RB, LB, LJButton, RJButton, Back, Start, Middle				#
-# RT, LT, LeftJoy, RightJoy, Left, Right, Up, Down					#
-#########################################################################################
+### Buttons/Joys Represented: #################################################
+# A, B, X, Y, RB, LB, LJButton, RJButton, Back, Start, Middle
+# RT, LT, LeftJoy, RightJoy, Left, Right, Up, Down
+###############################################################################
 
-### States Dictionary Reference: ########################################################
-#	self.states = { 'A':0, 'B':0, 'X':0, 'Y':0,  		   		\	#
-#			'Back':0, 'Start':0, 'Middle':0,		   	\	#
-#			'Left':0, 'Right':0, 'Up':0, 'Down':0, 			\	#
-#			'LB':0, 'RB':0, 'LT':0, 'RT':0,				\	#
-#			'LJ/Button':0, 'RJ/Button':0, 				\	#
-#			'LJ/Left':0, 'LJ/Right':0, 'LJ/Up':0, 'LJ/Down':0, 	\	#
-#			'RJ/Left':0, 'RJ/Right':0, 'RJ/Up':0, 'RJ/Down':0}		#
-#########################################################################################
+### States Dictionary Reference: ##############################################
+#	self.states = { 'A':0, 'B':0, 'X':0, 'Y':0,
+#			'Back':0, 'Start':0, 'Middle':0,
+#			'Left':0, 'Right':0, 'Up':0, 'Down':0,
+#			'LB':0, 'RB':0, 'LT':0, 'RT':0,
+#			'LJ/Button':0, 'RJ/Button':0,
+#			'LJ/Left':0, 'LJ/Right':0, 'LJ/Up':0, 'LJ/Down':0,
+#			'RJ/Left':0, 'RJ/Right':0, 'RJ/Up':0, 'RJ/Down':0}
+###############################################################################
 
 import sys
 import threading
 
 class ParserCore(threading.Thread):
 	def __init__(self, bus, states):
-		# Initializes threading
 		threading.Thread.__init__(self)
-		# Stores the bus and states objects
 		self.bus = bus
 		self.states = states
-		# Creates lists and dictionaries
+		# Creates templist for storing the 8-byte packages from gamepad
 		self.templist = []
 		# All button raw packet values of data coming from gamepad
-		self.buttons = {'\x00':'A', '\x01':'B', '\x02':'X', '\x03':'Y', \
-				'\x04':'LB', '\x05':'RB', '\x06':'Back', '\x07':'Start', \
+		self.buttons = {'\x00':'A', '\x01':'B', '\x02':'X', '\x03':'Y',		\
+				'\x04':'LB', '\x05':'RB', '\x06':'Back', '\x07':'Start',	\
 				'\x08':'Middle', '\t':'LJ/Button', '\n':'RJ/Button'}
 		# List of Joy Names
-		self.joys = ['LT', 'RT', 					\
-			     'LJ/Left', 'LJ/Right', 'LJ/Up', 'LJ/Down', 	\
-			     'RJ/Left', 'RJ/Right', 'RJ/Up', 'RJ/Down']
+		self.joys = ['LT', 'RT',									\
+					 'LJ/Left', 'LJ/Right', 'LJ/Up', 'LJ/Down', 	\
+					 'RJ/Left', 'RJ/Right', 'RJ/Up', 'RJ/Down']
 		# List of Byte names for states
-		self.bytes = ['Byte0', 'Byte1', 'Byte2', 'Byte3', 'Byte4', 'Byte5', 'Byte6', 'Byte7']
+		self.bytes = ['Byte0', 'Byte1', 'Byte2', 'Byte3',	\
+					  'Byte4', 'Byte5', 'Byte6', 'Byte7']
 		self.bytes_int = ['Byte0/INT', 'Byte1/INT', 'Byte2/INT', 'Byte3/INT', \
-			      'Byte4/INT', 'Byte5/INT', 'Byte6/INT', 'Byte7/INT']
+						  'Byte4/INT', 'Byte5/INT', 'Byte6/INT', 'Byte7/INT']
 		# Initializes templist
 		for x in range(8):
 		        self.templist.append(0)
@@ -96,7 +95,8 @@ class ParserCore(threading.Thread):
 
 	def parse_pressed_button(self):
 		# Updates states of buttons to 1 (on)
-		if self.templist[6]=='\x01' and self.templist[5]=='\x00': # Letters, Start/Back
+		if self.templist[6]=='\x01' and self.templist[5]=='\x00': # Letters,
+																  # Start/Back
 			self.states[self.buttons[self.templist[7]]] = 1
 		elif self.templist[6]=='\x02' and self.templist[5]=='\x80': # D-Pad L/U
 			if self.templist[7]=='\x06': # Left
@@ -125,28 +125,28 @@ class ParserCore(threading.Thread):
 			elif ord(self.templist[5])<=127:
 				val = ord(self.templist[5]) + 127
 				self.states['RT'] = val
-		elif self.templist[7]=='\x00' and self.templist[6]=='\x02': # Left-Joy L/R
+		elif self.templist[7]=='\x00' and self.templist[6]=='\x02': # LJ L/R
 			if ord(self.templist[5])<=127: # Right
 				val = ord(self.templist[5])
 				self.states['LJ/Right'] = val
 			elif ord(self.templist[5])>=128: # Left
 				val = ord(self.templist[5]) - 255
 				self.states['LJ/Left'] = val
-		elif self.templist[7]=='\x01' and self.templist[6]=='\x02': # Left-Joy U/D
+		elif self.templist[7]=='\x01' and self.templist[6]=='\x02': # LJ U/D
 			if ord(self.templist[5])<=127: # Down
 				val = -(ord(self.templist[5]))		# Flip to negative
 				self.states['LJ/Down'] = val
 			elif ord(self.templist[5])>=128: # Up
 				val = (ord(self.templist[5]) - 255) * -1	# Flip to positive
 				self.states['LJ/Up'] = val
-		elif self.templist[7]=='\x03' and self.templist[6]=='\x02': # Right-Joy L/R
+		elif self.templist[7]=='\x03' and self.templist[6]=='\x02': # RJ L/R
 			if ord(self.templist[5])<=127: # Right
 				val = ord(self.templist[5])
 				self.states['RJ/Right'] = val
 			elif ord(self.templist[5])>=128: # Left
 				val = ord(self.templist[5]) - 255
 				self.states['RJ/Left'] = val
-		elif self.templist[7]=='\x04' and self.templist[6]=='\x02': # Right-Joy U/D
+		elif self.templist[7]=='\x04' and self.templist[6]=='\x02': # RJ U/D
 			if ord(self.templist[5])<=127: # Down
 				val = -(ord(self.templist[5]))		# Flip to negative
 				self.states['RJ/Down'] = val
@@ -156,9 +156,9 @@ class ParserCore(threading.Thread):
 
 	def parse_released_button(self):
 		# Updates states of buttons to 0(off)
-		if self.templist[5]=='\x00' and self.templist[6]=='\x01': # Letters, Start/Stop, or LJ/RJ Buttons
-			# Check to see if templist[5] is in button list
-			# and if not then see if it is /00 - DPad
+		if self.templist[5]=='\x00' and self.templist[6]=='\x01': # Letters,
+																  # Start/Stop,
+																  # L/RJ Button
 			self.states[self.buttons[self.templist[7]]] = 0
 		elif self.templist[5]=='\x00' and self.templist[6]=='\x02': # D-Pad
 			if self.templist[7]=='\x07': # D-Pad, Up/Down
@@ -171,4 +171,3 @@ class ParserCore(threading.Thread):
 					self.states['Left'] = 0
 				elif self.states['Right']==1: # Right
 					self.states['Right'] = 0
-		# ADD IN JOY RELEASED
--- parser_main.py
+++ parser_main.py
@@ -1,22 +1,23 @@
 ############ Logitech F310 Gamepad Controller Main - parser_main.py ##########
 # Original Author: John Zeller
-# Description: Parser_Main polls the data in a python dictionary to check the states of
-# 	       several buttons coming from parser_core.py. Once it reads these states, it
-#	       will display them for reference on the terminal it was launched in
+# Description: Parser_Main polls the data in a python dictionary to check the
+#			   states of several buttons coming from parser_core.py. Once it
+#			   reads these states, it will display them for reference on the
+#			   terminal it was launched in
 
-### NOTES ###############################################################################
-# 1) LEAVE MODE 'OFF' there is no support in parser_main.py for MODE ON 		#
-# 2) Naturally the gamepad sends the following values:					#
-# 	LJ/RJ - Down: 0-127								#
-# 	LJ/RJ - Up: 255-128								#
-# 	LJ/RJ - Left: 255-128								#
-# 	LJ/RJ - Right: 0-127								#
-#########################################################################################
+### NOTES #####################################################################
+# 1) LEAVE MODE 'OFF' there is no support in parser_main.py for MODE ON
+# 2) Naturally the gamepad sends the following values:
+# 	LJ/RJ - Down: 0-127
+# 	LJ/RJ - Up: 255-128
+# 	LJ/RJ - Left: 255-128
+# 	LJ/RJ - Right: 0-127
+###############################################################################
 
-### Buttons/Joys Represented: ###########################################################
-# A, B, X, Y, RB, LB, LJButton, RJButton, Back, Start, Middle				#
-# RT, LT, LeftJoy, RightJoy, Left, Right, Up, Down					#
-#########################################################################################
+### Buttons/Joys Represented: #################################################
+# A, B, X, Y, RB, LB, LJButton, RJButton, Back, Start, Middle
+# RT, LT, LeftJoy, RightJoy, Left, Right, Up, Down
+###############################################################################
 
 import sys
 sys.path.append("./core")
@@ -33,14 +34,14 @@ class ParserMain(threading.Thread):
 		self.bus = Bus()
 		# Create a dictionary to be used to keep states from joy_core
 		self.states = { 'A':0, 'B':0, 'X':0, 'Y':0,  		   		\
-				'Back':0, 'Start':0, 'Middle':0,		   	\
-				'Left':0, 'Right':0, 'Up':0, 'Down':0, 			\
-				'LB':0, 'RB':0, 'LT':0, 'RT':0,				\
-				'LJ/Button':0, 'RJ/Button':0, 				\
+				'Back':0, 'Start':0, 'Middle':0,		   			\
+				'Left':0, 'Right':0, 'Up':0, 'Down':0, 				\
+				'LB':0, 'RB':0, 'LT':0, 'RT':0,						\
+				'LJ/Button':0, 'RJ/Button':0, 						\
 				'LJ/Left':0, 'LJ/Right':0, 'LJ/Up':0, 'LJ/Down':0, 	\
 				'RJ/Left':0, 'RJ/Right':0, 'RJ/Up':0, 'RJ/Down':0,	\
-				'Byte0':0, 'Byte1':0, 'Byte2':0, 'Byte3':0,		\
-				'Byte4':0, 'Byte5':0, 'Byte6':0, 'Byte7':0,		\
+				'Byte0':0, 'Byte1':0, 'Byte2':0, 'Byte3':0,			\
+				'Byte4':0, 'Byte5':0, 'Byte6':0, 'Byte7':0,			\
 				'Byte0/INT':0, 'Byte1/INT':0, 'Byte2/INT':0, 		\
 				'Byte3/INT':0, 'Byte4/INT':0, 'Byte5/INT':0, 		\
 				'Byte6/INT':0, 'Byte7/INT':0}
@@ -49,11 +50,16 @@ class ParserMain(threading.Thread):
 		self.parsercore.start()
 	
 	def run(self):
-		# Description: Polls the gamepad states and displays their current values on a simple Tkinter GUI
+		# Description: Polls the gamepad states and displays their current
+		#			   values on a simple Tkinter GUI
 		
 		# Launch simple GUI with Tkinter (Native GUI on Python)
 		gui = GUI(self.states)     # Create GUI object
-		gui.mainloop()  # Launch GUI object into the main loop
+		try:
+			gui.mainloop()  # Launch GUI object into the main loop
+		except AttributeError as ex: # If mainloop is broken then sys.quit
+			print "Found AttributeError, closing debugger"
+			# Now Exit
 
 if __name__ == '__main__':
         parser = ParserMain()
